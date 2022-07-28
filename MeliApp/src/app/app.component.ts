5aafd37e4915b5539a5fa1b28bbe7e1228ebd0be
lcerbino <cerbino.lucas@gmail.com>
2022-07-28 11:45:57 -0300

    added: " first components"

--- MeliApp/src/app/app.component.ts
+++ MeliApp/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MeliApp2';
+export class AppComponent implements OnInit{
+  
+  title : string | undefined;
+  
+  ngOnInit(): void {
+    this.title = 'MeliApp2';
+  }
+
 }
